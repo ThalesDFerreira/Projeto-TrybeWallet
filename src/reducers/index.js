945c7b2ea331e751fc9361e2d83da2fb75b3061d
ThalesFerreira <thalesferreira190@gmail.com>
2022-06-22 14:43:32 -0300

    configurações iniciais

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,17 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+import { combineReducers } from 'redux';
+
+const INITIAL_STATE = {};
+
+const exampleReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+const rootReducer = combineReducers({ exampleReducer });
+
+export default rootReducer;
