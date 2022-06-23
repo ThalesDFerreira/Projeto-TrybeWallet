d3f3d1c5109a505950f5cadfae64cd0f79fd2a77
ThalesFerreira <thalesferreira190@gmail.com>
2022-06-22 21:40:19 -0300

    Correções Saturnino Branch Master 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 export const EMAIL_ACTION = 'EMAIL_ACTION';
-export const NEW_ACTION2 = 'NEW_ACTION2';
+// export const NEW_ACTION2 = 'NEW_ACTION2';
 
-export const infoUser = (email) => ({ type: EMAIL, email });
+export const infoUser = (email) => ({ type: EMAIL_ACTION, email: email });
 
-export const infoUser2 = (state) => ({ type: NEW_ACTION2, payload: state });
+// export const infoUser2 = (state) => ({ type: NEW_ACTION2, payload: state });
